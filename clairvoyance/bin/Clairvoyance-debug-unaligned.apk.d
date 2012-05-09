f712f898f7f21e2c9a4947da1360ff3753de6741
Robert Deaton <rdeaton@udel.edu>
2012-05-09 17:07:31 -0400

    Accidentally committed built things.
    Signed-off-by: Robert Deaton <rdeaton@udel.edu>

--- clairvoyance/bin/Clairvoyance-debug-unaligned.apk.d
+++
@@ -1,9 +0,0 @@
-/Users/rdeaton/dev/cpeg454/clairvoyance/bin/Clairvoyance-debug-unaligned.apk : \
-/Users/rdeaton/dev/cpeg454/clairvoyance/bin/Clairvoyance.ap_ \
-/Users/rdeaton/dev/cpeg454/clairvoyance/bin/classes.dex \
-/Users/rdeaton/dev/cpeg454/clairvoyance/libs/locale_platform.jar \
-/Users/rdeaton/dev/android/android-sdk-macosx/tools/support/annotations.jar \
-/Users/rdeaton/dev/cpeg454/clairvoyance/libs/guava-r06.jar \
-/Users/rdeaton/dev/cpeg454/clairvoyance/libs/libGoogleAnalytics.jar \
-/Users/rdeaton/dev/cpeg454/clairvoyance/libs/script.jar \
-/Users/rdeaton/dev/cpeg454/clairvoyance/libs/armeabi/libcom_googlecode_android_scripting_Exec.so \
