026fba69577119361b64f9cce03bf3110412f3a6
Robert Deaton <rdeaton@udel.edu>
2012-05-09 16:49:00 -0400

    App now starts up the GPS sensor
    Signed-off-by: Robert Deaton <rdeaton@udel.edu>

--- clairvoyance/res/raw/libs/common.py
+++ clairvoyance/res/raw/libs/common.py
@@ -2,3 +2,43 @@
 droid = None
 # Holds the path to the base directory
 path = ''
+# Holds the current state of the GPS sensor
+gps_enabled = None
+
+def try_to_enable_gps(prompt = False):
+    """
+    Attempts to enable the GPS sensor. If prompt is false, it will pull up the
+    settings pane without asking if GPS is off on the first attempt to enable.
+    """
+    global gps_enabled
+    while True:
+        # Let's make sure GPS is on
+        droid.dialogCreateSpinnerProgress("Clairvoyance", "Waiting for GPS...")
+        droid.dialogShow()
+        droid.startLocating()
+        droid.eventWaitFor('location')
+        droid.dialogDismiss()
+    
+        location = droid.readLocation().result
+        if location == {} or 'gps' not in location:
+            # We couldn't track the location
+            if prompt:
+                droid.dialogCreateAlert("GPS is not currently enabled. Enable it now?")
+                droid.dialogSetPositiveButtonText("Yes")
+                droid.dialogSetNegativeButtonText("No")
+                droid.dialogShow()
+            else:
+                prompt = True
+        
+            response = droid.dialogGetResponse().result
+            if 'which' in response and response['which'] == 'positive':
+                intent = droid.makeIntent(None, None, None, None, None, "com.android.settings", "com.android.settings.SecuritySettings", 0).result
+                droid.startActivityForResultIntent(intent)
+            else:
+                droid.stopLocating()
+                gps_enabled = False
+                return False
+        else:
+            droid.stopLocating()
+            gps_enabled = True
+            return True
--- clairvoyance/res/raw/script.py
+++ clairvoyance/res/raw/script.py
@@ -16,7 +16,7 @@ def event_loop():
         event = droid.eventWait().result
         print event
         # droid.fullQueryDetail("textview1").result['text'] = str(event)
-        time.sleep(5)
+        time.sleep(1)
         return
 
 if __name__ == '__main__':
@@ -27,32 +27,13 @@ if __name__ == '__main__':
         droid = sl4a.Android()
     common.droid = droid
     common.path = path
-
+    
+    common.try_to_enable_gps()
+    
+    
+    
     layout = open(os.path.join(path, 'layouts', 'main.xml'), 'r').read()
     droid.fullShow(layout)
-    
-    # Let's make sure GPS is on
-    droid.startLocating()
-    droid.eventWaitFor('location')
-    
-    location = droid.readLocation().result
-    if location == {} or 'gps' not in location:
-        # We couldn't track the location
-        droid.dialogCreateAlert("GPS is not current enabled. Enable it now?")
-        droid.setPositiveButtonText("Yes")
-        droid.setNegativeButtonText("No")
-        droid.dialogShow()
-        
-        droid.eventWaitFor('dialog')
-    
-    
-    
-    
-    intent = droid.makeIntent(None, None, None, None, None, "com.android.settings", "com.android.settings.SecuritySettings", 0).result
-    droid.startActivityForResultIntent(intent)
-    
-    
-    
     
     event_loop()
     droid.fullDismiss()
