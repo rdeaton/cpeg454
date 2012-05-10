c8785c910e2476f9285ca0264240626a4fe15cb8
Robert Deaton <rdeaton@udel.edu>
2012-05-10 00:36:16 -0400

    Actually save the slider settings.
    Signed-off-by: Robert Deaton <rdeaton@udel.edu>

--- clairvoyance/res/raw/layouts/settings.py
+++ clairvoyance/res/raw/layouts/settings.py
@@ -1,11 +1,29 @@
 import manager
 import common
 import os
+import json
 from common import droid
 
+settings = {
+     'scan_interval' : 60,
+     'throughput_interval' : 60,
+     'minimum_battery' : 15}
+
+def save_settings():
+    droid.fullSetProperty("currentScanInterval", "text", str(settings['scan_interval']) + 's')
+    droid.fullSetProperty("currentThroughputInterval", "text", str(settings['throughput_interval']) + 's')
+    droid.fullSetProperty("currentMinimumBattery", "text", str(settings['minimum_battery']) + '%')     
+    droid.prefPutValue('settings', json.dumps(settings), 'clairvoyance')
+
 def open_view():
+    global settings
     layout = open(os.path.join(common.path, 'layouts', 'settings.xml'), 'r').read()
     droid.fullShow(layout)
+    
+    prefs = droid.prefGetValue('settings', 'clairvoyance').result
+    if prefs is not None:
+        settings = json.loads(prefs)
+    save_settings() # Updates the UI, and places the defaults in the keystore if nothing was loaded
     
     droid.addOptionsMenuItem("Close Application","EXIT_APP",None,"star_on")
     info = droid.wifiGetConnectionInfo().result
@@ -13,6 +31,7 @@ def open_view():
         droid.fullSetProperty("currentNetworkSSID", "text", "Current WiFi Network SSID: " + droid.wifiGetConnectionInfo().result['ssid'])
     else:
         droid.fullSetProperty("currentNetworkSSID", "text", "Current WiFi Network SSID: NOT CONNECTED or CARD DISABLED")
+    
 
 def close_view():
     droid.fullDismiss()
@@ -26,43 +45,31 @@ def handle_event(event):
     if event["name"]=="click":
         id = event["data"]["id"]
         if id == "openScanSlider":
-            #create the seek bar with the current value in for the start value
-            current = droid.fullQueryDetail("currentScanInterval").result['text']
-            droid.dialogCreateSeekBar(str(current),100,"Scan Interval","How many seconds should the phone wait in between WiFi Scans?  A smaller interval will decrease battery life but increase data collection.")
+            print settings
+            print type(settings)
+            print settings['scan_interval']
+            droid.dialogCreateSeekBar(settings['scan_interval'], 100, "Scan Interval", "How many seconds should the phone wait in between WiFi Scans?  A smaller interval will decrease battery life but increase data collection.")
             droid.dialogSetPositiveButtonText("Update Interval")
             droid.dialogShow()
             sliderResp = droid.dialogGetResponse().result
-            currentSliderValue = sliderResp['progress']
-            #event=droid.eventWait().result
-            #data = event['data']
-            #number = data['progress']
-            droid.fullSetProperty("currentScanInterval", "text", str(currentSliderValue))
+            settings['scan_interval'] = sliderResp['progress']
+            save_settings()
             return manager.EVENT_CONSUME
         elif id == "openThroughputSlider":
-            #create the seek bar with the current value in for the start value
-            current = droid.fullQueryDetail("currentThroughputInterval").result['text']
-            droid.dialogCreateSeekBar(str(current),100,"Throughput Interval","How many seconds should the phone wait in between throughput tests?  A smaller interval will decrease battery life but increase data collection.")
+            droid.dialogCreateSeekBar(str(settings['throughput_interval']), 100, "Throughput Interval", "How many seconds should the phone wait in between throughput tests?  A smaller interval will decrease battery life but increase data collection.")
             droid.dialogSetPositiveButtonText("Update Interval")
             droid.dialogShow()
             sliderResp = droid.dialogGetResponse().result
-            currentSliderValue = sliderResp['progress']
-            #event=droid.eventWait().result
-            #data = event['data']
-            #number = data['progress']
-            droid.fullSetProperty("currentThroughputInterval", "text", str(currentSliderValue))
+            settings['throughput_interval'] = sliderResp['progress']
+            save_settings()
             return manager.EVENT_CONSUME
         elif id == "minimumBattery":
-            #create the seek bar with the current value in for the start value
-            current = droid.fullQueryDetail("currentMinimumBattery").result['text']
-            droid.dialogCreateSeekBar(str(current),100,"Minimum Battery Level","This app can be configured to stop scanning when battery level is below a certain level.  Choose the minimum battery level.")
+            droid.dialogCreateSeekBar(str(settings['minimum_battery']), 100, "Minimum Battery Level", "This app can be configured to stop scanning when battery level is below a certain level.  Choose the minimum battery level.")
             droid.dialogSetPositiveButtonText("Update Minimum battery level")
             droid.dialogShow()
             sliderResp = droid.dialogGetResponse().result
-            currentSliderValue = sliderResp['progress']
-            #event=droid.eventWait().result
-            #data = event['data']
-            #number = data['progress']
-            droid.fullSetProperty("currentMinimumBattery", "text", str(currentSliderValue)) 
+            settings['minimum_battery'] = sliderResp['progress']
+            save_settings()
             return manager.EVENT_CONSUME
     elif event["name"]=="screen":
         if event["data"]=="destroy":
