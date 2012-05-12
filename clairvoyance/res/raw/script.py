7a37114036a8b18320e9ede68893ce0e6d227f7d
Robert Deaton <rdeaton@udel.edu>
2012-05-12 12:58:43 -0400

    Add a support.zip file that gets created automatically at build time for the APK, since we can't embed a directory structure within res/raw.
    Signed-off-by: Robert Deaton <rdeaton@udel.edu>

--- clairvoyance/res/raw/script.py
+++ clairvoyance/res/raw/script.py
@@ -1,17 +1,28 @@
 import time
 import os
 import sys
-
-path = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, path + '/libs')
-import fix_json
-try:
-    import android as sl4a
-except ImportError:
-    import sl4a
+import zipfile
 
 if __name__ == '__main__':
+    path = os.path.dirname(os.path.abspath(__file__))
+    # We need to unpackage some things
+    support = zipfile.ZipFile(os.path.join(path, "support.zip"), 'r')
+    support.extractall(path)
+
+    # Add the librares to our path
+    sys.path.insert(0, path + '/libs')
+    # fix_json casts all the unicode strings to ascii, for convenience
+    import fix_json
+    # On non-android systems, sl4a is the android module
+    try:
+        import android as sl4a
+    except ImportError:
+        import sl4a
+
+    # In common we store a number of things shared throughout the program
     import common
+    # On non-android systems, we allow a host and port to be passed for where
+    # to connect to the RPC server
     if len(sys.argv) == 3:
         addr = sys.argv[1], sys.argv[2]
         droid = sl4a.Android(addr)
@@ -19,14 +30,12 @@ if __name__ == '__main__':
         droid = sl4a.Android()
     common.droid = droid
     common.path = path
+
     common.load_views()
     import manager
-    
-    
-    manager.push_view(common.views['settings'])
-    
-    manager.main_loop()
-    
+
+
     # Turn on GPS at app startup
     common.gps_lock()
-    
+    manager.push_view(common.views['settings'])
+    manager.main_loop()
