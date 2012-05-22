cfe0e28b756bfdd5beba38242ff273f865e076d3
Robert Deaton <rdeaton@udel.edu>
2012-05-22 13:29:55 -0400

    Fix support files in apk builds
    Signed-off-by: Robert Deaton <rdeaton@udel.edu>

--- clairvoyance/res/raw/script.py
+++ clairvoyance/res/raw/script.py
@@ -6,8 +6,15 @@ import zipfile
 if __name__ == '__main__':
     path = os.path.dirname(os.path.abspath(__file__))
     # We need to unpackage some things
-    #support = zipfile.ZipFile(os.path.join(path, "support.zip"), 'r')
-    #support.extractall(path)
+    try:
+        support = zipfile.ZipFile(os.path.join(path, "support.zip"), 'r')
+        support.extractall(path)
+    except OSError:
+        import shutil
+        shutil.rmtree(os.path.join(path, 'libs'))
+        shutil.rmtree(os.path.join(path, 'layouts'))
+        support = zipfile.ZipFile(os.path.join(path, "support.zip"), 'r')
+        support.extractall(path)
 
     # Add the librares to our path
     sys.path.insert(0, path + '/libs')
